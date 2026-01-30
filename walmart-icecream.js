9e3a22a5648c4b0773d60c4d6704c0ad7918023c
Mike73-hub <mike_buday@yahoo.com>
2026-01-30 12:14:29 -0600

    Create walmart-icecream.js

---
+++ walmart-icecream.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+
+    res.status(200).json({
+      quote: data.content,
+      author: data.author
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch quote" });
+  }
+}
