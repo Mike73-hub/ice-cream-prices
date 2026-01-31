6084d7f52330f9c1e3ac24231795c032e6e29f55
Mike73-hub <mike_buday@yahoo.com>
2026-01-31 17:08:16 -0600

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -2,22 +2,20 @@ export default async function handler(req, res) {
   try {
     const response = await fetch("https://www.walmart.com/ip/154450819", {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
-        "Accept-Language": "en-US,en;q=0.9"
-        "Upgrade-Insecure-Requests": "1"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Upgrade-Insecure-Requests": "1",
         "Sec-Fetch-Site": "none",
         "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Dest": "document"
-        "Sec-Fetch-User": "?1"
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-User": "?1",
         "Connection": "keep-alive",
-        "Cache-Control": "max-age=0"
-        "Connection": "keep-alive",
-        "Cache-Control": "max-age=0"
-        "Accept-Encoding": "gzip, deflate, br"
+        "Cache-Control": "max-age=0",
+        "Accept-Encoding": "gzip, deflate, br",
         "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
         "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\""
+        "sec-ch-ua-platform": "\"Windows\"",
         "Referer": "https://www.walmart.com/"
       }
     });
