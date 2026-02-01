a39878efb7eadbe19671326dee07b181e00d0b03
Mike73-hub <mike_buday@yahoo.com>
2026-01-31 18:16:32 -0600

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://www.walmart.com/ip/154450819", {
+    const response = await fetch("https://www.walmart.com/ip/193361645", {
       headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
